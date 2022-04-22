50b4ea43d66f3612d5244ff8a817d3a5387934a6
41023112 <41023112@gm.nfu.edu.tw>
2022-04-22 11:49:54 +0800

    midterm

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '41023112嘿嘿嘿', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '41023112嘿嘿嘿 \n', 'tags': '', 'url': 'About.html'}, {'title': 'midterm', 'text': '\n \n \n   \n \n \n \n \n \n \n \n', 'tags': '', 'url': 'midterm.html'}]};
